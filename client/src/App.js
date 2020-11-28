60af5f88cc7fcc49fabc5e0cbb6d029ed81f9e27
sho11hei12-1998 <sho11hei12@docomo.ne.jp>
2020-11-28 13:15:32 +0900

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,71 +1,218 @@
-import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import React from "react";
+import Resister from "./Resister.json";
 import getWeb3 from "./getWeb3";
+
+import { Row, Col, Button, Form, Modal } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./App.css";
 
-class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      web3: null,
+      accounts: null,
+      contract: null,
+      name: null,
+      age: null,
+      hobby: null,
+      address: "",
+      outputName: null,
+      outputAge: null,
+      outputHobby: null,
+
+      ////
+      lines: [],
+
+      // モーダル
+      show: false,
+      // フォームチェック
+      validated: false,
+    };
+  }
+
+  // モーダル設定
+  handleClose = async () => {
+    await this.setState({ show: false });
+
+    // ページリロード
+    document.location.reload();
+  }
+
+  handleShow = async () => this.setState({ show: true });
 
   componentDidMount = async () => {
     try {
-      // Get network provider and web3 instance.
       const web3 = await getWeb3();
 
-      // Use web3 to get the user's accounts.
       const accounts = await web3.eth.getAccounts();
-
-      // Get the contract instance.
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
+      const deployedNetwork = Resister.networks[networkId];
       const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
+        Resister.abi,
+        deployedNetwork && deployedNetwork.address
       );
 
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      this.setState({ web3, accounts, contract: instance });
     } catch (error) {
-      // Catch any errors for any of the above operations.
       alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
+        `Failed to load web3, accounts, or contract. Check console for details.`
       );
       console.error(error);
     }
-  };
 
-  runExample = async () => {
     const { accounts, contract } = this.state;
+    console.log(accounts);
 
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
+    const item = await contract.methods.accounts(accounts[0]).call();
+    this.state.lines.push({
+      item
+    });
 
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
+    console.log(this.state.lines);
   };
+
+  // アカウント情報の登録
+  writeRecord = async () => {
+    const { accounts, contract, name, age, hobby } = this.state;
+    const result = await contract.methods.registerAccount(name, age, hobby).send({
+      from: accounts[0],
+    });
+    console.log(result);
+
+    if (result.status === true) {
+      this.handleShow();
+    }
+  };
+
+  // アカウント情報の読み込み
+  viewRecord = async () => {
+    const { contract, accounts } = this.state;
+    console.log(contract);
+
+    const result = await contract.methods.viewAccount(accounts[0]).call();
+    console.log(result);
+
+    const outputName = result[0];
+    const outputAge = result[1];
+    const outputHobby = result[2];
+    this.setState({ outputName, outputAge, outputHobby });
+  };
+
+  handleChange = (name) => (event) => {
+    this.setState({ [name]: event.target.value });
+  };
+
+  // フォーム最終確認
+  handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    this.setState({ validated: true });
+  };
+
 
   render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
-      </div>
+        <Row className="text-left m-5">
+          <Col md={{ span: 4, offset: 2 }}>
+            <Form className="justify-content-center"
+              noValidate validated={this.state.validated} >
+
+              <Form.Group controlId="validationCustom03">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  type="name"
+                  onChange={this.handleChange("name")}
+                  placeholder="Enter Name"
+                  required />
+                <Form.Control.Feedback type="invalid">
+                  Please enter name.
+                </Form.Control.Feedback>
+              </Form.Group>
+
+              <Form.Group controlId="validationCustom03">
+                <Form.Label>Age</Form.Label>
+                <Form.Control
+                  type="text"
+                  onChange={this.handleChange("age")}
+                  placeholder="Enter Age"
+                  required />
+                <Form.Text className="text-muted">
+                  We'll never share your age with anyone else.
+                </Form.Text>
+                <Form.Control.Feedback type="invalid">
+                  Please enter age.
+                </Form.Control.Feedback>
+              </Form.Group>
+
+              <Form.Group controlId="validationCustom03">
+                <Form.Label>Hobby</Form.Label>
+                <Form.Control
+                  type="text"
+                  onChange={this.handleChange("hobby")}
+                  placeholder="Enter Hobby"
+                  required />
+                <Form.Control.Feedback type="invalid">
+                  Please enter hobby.
+                </Form.Control.Feedback>
+              </Form.Group>
+
+              {/* フォームチェック */}
+              <Form.Group>
+                <Form.Check
+                  required
+                  label="Agree to terms and conditions"
+                  feedback="You must agree before submitting."
+                  onChange={this.handleSubmit}
+                />
+              </Form.Group>
+
+              <Button variant="primary" type="submit" onClick={this.writeRecord}>
+                会員登録
+              </Button>
+
+              {/* モーダル */}
+              <Modal show={this.state.show} onHide={this.handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Wellcome to BcDiary!!</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>会員登録が完了しました。</Modal.Body>
+                <Modal.Footer>
+                  <Button variant="secondary" onClick={this.handleClose}>
+                    Close
+                  </Button>
+                </Modal.Footer>
+              </Modal>
+            </Form>
+          </Col>
+
+          <Col md={{ span: 4, offset: 0 }} className='ml-5'>
+            <Form className="justify-content-center">
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>検索したいアドレスを入力してください。</Form.Label>
+                <Form.Control onChange={this.handleChange("address")}
+                  placeholder="Search" />
+              </Form.Group>
+              <Button variant="primary" type="submit" onClick={this.viewRecord}>
+                閲覧
+          </Button>
+            </Form>
+
+            <br />
+            <br />
+
+            {this.state.outputName ? <p>Name: {this.state.outputName}</p> : <p></p>}
+            {this.state.outputAge ? <p>Age: {this.state.outputAge}</p> : <p></p>}
+            {this.state.outputHobby ? <p>Hobby: {this.state.outputHobby}</p> : <p></p>}
+
+          </Col>
+        </Row>
+      </div >
     );
   }
 }
